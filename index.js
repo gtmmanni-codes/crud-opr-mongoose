0a4c9fbbe2f2508ba2a7248be85809abc2b35ca6
gtmmanni-codes <gtmmanni@gmail.com>
2026-05-10 15:49:40 +0200

    trying to connect to mongoose & establishing connection

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let express = require("express");
+let mongoose = require ("mongoose");
+require('dotenv').config();
+ 
+//connect to mongoose
 
 let app = express();
+
+mongoose.connect(process.env.DBURL);
+
 
 app.use(express.json());
 
