05061fccc7cac63a6234a86962282f33974d38a4
gtmmanni-codes <gtmmanni@gmail.com>
2026-05-11 23:09:32 +0200

    adding data

--- models/enquiry.model.js
+++ models/enquiry.model.js
@@ -1,2 +1,14 @@
 let mongoose = require("mongoose");
-let userEnquirySchema = mongoose.Schema({}); 
+let userEnquirySchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required : true,
+        unique:true
+ 
+    }
+    
+}); 
