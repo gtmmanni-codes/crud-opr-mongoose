4ace28d9c18aded5ec4adf558a1a32447d2caf40
gtmmanni-codes <gtmmanni@gmail.com>
2026-05-12 23:51:39 +0200

    comments

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.post('api/enquiry-insert',(req,res)=>{
     console.log("working")
 })
 
+
+//mongoose connect
 mongoose.connect(process.env.DBURL).then(()=>{
     console.log("connected to MongoDB");
     app.listen(process.env.PORT,()=>{
